05dab285579b3c3531af618217724ce645e5e3eb
DenisEsipko <iesypko@gmail.com>
2024-11-24 18:41:15 +0200

    rewrote all code into functional components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 
 
@@ -6,6 +6,7 @@ function Search(props) {
 
     let [search, setSearch] = useState('');
     let [type, setType] = useState('all');
+   
 
     let handleKey = (event) => {
         if (event.key === "Enter") {
@@ -18,7 +19,7 @@ function Search(props) {
     }
 
     useEffect(() => {
-        props.searchMovies(search, type)
+            props.searchMovies(search, type)
     }, [type]);
 
 
